759a6bf1f331e9b780f5a21454262441d4d15e5e
itgyu <itgyu@users.noreply.github.com>
2025-11-19 14:10:49 +0900

    Add Firebase Storage export

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApps } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: "AIzaSyB-KFGyCaCi331p3wqIQ5M6xjlQmoxnL3I",
@@ -16,4 +17,5 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0
 
 export const auth = getAuth(app)
 export const db = getFirestore(app)
+export const storage = getStorage(app)
 export default app
